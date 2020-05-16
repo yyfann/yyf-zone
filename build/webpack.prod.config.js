da547e351bbd7944241953856d260e31f374b152
yyfann <a1097833559@163.com>
2020-05-16 13:53:11 +0800

    gzip

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const webpackMerge = require('webpack-merge')
-
 const commonConfig = require('./webpack.common.config')
+
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 const devConfig = {
   mode: "production",
@@ -17,6 +18,19 @@ const devConfig = {
       },
     ]
   },
+  plugins: [
+    new CompressionWebpackPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: new RegExp('\\.(js|css|html)$'),
+      // 只处理大于xx字节 的文件，默认：0
+      threshold: 10,
+      // 示例：一个1024b大小的文件，压缩后大小为768b，minRatio : 0.75
+      minRatio: 0.8, // 默认: 0.8
+      // 是否删除源文件，默认: false
+      deleteOriginalAssets: false
+    })
+  ],
 
 };
 
